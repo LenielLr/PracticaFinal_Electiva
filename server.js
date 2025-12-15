040f3dd1c2b3f2448d66a92e0f74c38e80cadfa4
Leniel Bello <yamichen62@gmail.com>
2025-12-14 23:37:31 -0400

    Cambio

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+const app = require("./src/app");
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Running on port ${PORT}`);
+});
